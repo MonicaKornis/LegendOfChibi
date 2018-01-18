c78a283d21498edaf53b1ef7d522ffe8d14adbc6
monicakornis <2011mkornis@gmail.com>
2018-01-18 16:26:13 -0500

    adding game circle and background classes

---
+++ bundle.js
@@ -0,0 +1,264 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const Game = __webpack_require__(1);
+
+document.addEventListener('DOMContentLoaded', () => {
+  // debugger
+  let gameCanvas = document.getElementById('game-canvas');
+  let ctx;
+
+  if (gameCanvas.getContext) {
+    ctx = gameCanvas.getContext('2d');
+    let game = new Game(12,12);
+    game.start(ctx);
+  }
+
+});
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+let Circle = __webpack_require__(2);
+let Background = __webpack_require__(3);
+
+class Game {
+  constructor(xDim, yDim) {
+    this.xDim = xDim;
+    this.yDim = yDim;
+  }
+
+  renderBackground(ctx) {
+    new Background(0,0).draw(ctx);
+  }
+
+
+  start(ctx) {
+    this.renderBackground(ctx);
+    this.draw(ctx);
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = "red";
+    let c = new Circle(this.xDim,this.yDim,10,'blue');
+    // b.width = 25;
+    // b.height = 25;
+    // debugger
+    // ctx.drawImage(b, 0, 0, 600, 525);
+    ctx.fillRect(25, 25, 25, 25);
+
+    // b.draw(ctx);
+    c.draw(ctx);
+
+    // debugger
+
+    const animateCallback = () => {
+      debugger
+      ctx.clearRect(0, 0, 620, 550);
+        if (this.xDim < 200 && this.yDim < 200 ) {
+        this.xDim += 1;
+        this.yDim += 1;
+        this.draw(ctx);
+      } else if (this.xDim >= 200 && this.yDim >= 200) {
+        this.xDim = 1;
+        this.yDim = 1;
+        this.draw(ctx);
+      }
+    };
+
+    window.requestAnimationFrame(animateCallback);
+  }
+
+  moveCircle(c) {
+    c.moveRandom(this.xDim , this.yDim);
+  }
+
+}
+
+module.exports = Game;
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+class Circle {
+  constructor(centerX, centerY, radius, color) {
+    this.centerX = centerX;
+    this.centerY = centerY;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  randomCircle(maxX, maxY, numCircles) {
+    return new Circle(
+      maxX * Math.random(),
+      maxY * Math.random(),
+      Circle.radius(maxX, maxY, numCircles),
+      this.randomColor()
+    );
+  }
+
+  radius(maxX, maxY, numCircles) {
+    let targetCircleArea = (maxX * maxY) / numCircles;
+    let targetRadius = Math.sqrt(targetCircleArea/ Math.PI);
+    return 2 * targetRadius;
+  }
+
+  randomColor() {
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += hex[Math.floor((Math.random() * 16))];
+    }
+    return color;
+  }
+
+  moveRandom(maxX, maxY) {
+    let dy = (Math.random() * 2) - 1;
+    let dx = (Math.random() * 2) - 1;
+
+    this.centerX = Math.abs((this.centerX + (dx * this.radius * .1)) % maxX);
+    this.centerY = Math.abs((this.centerY + (dx * this.radius * .1)) % maxY);
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+
+    ctx.arc(
+      this.centerX,
+      this.centerY,
+      this.radius,
+      0,
+      2 * Math.PI,
+      false
+    );
+
+    ctx.fill();
+  }
+}
+
+module.exports = Circle;
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+class Background {
+  contructor(posY, imageLength, speed) {
+
+    this.speed = 10;
+    this.y = posY;
+    this.totalSeconds = 10;
+    this.x = 0;
+    this.imageLength = imageLength;
+  }
+
+
+  draw(ctx) {
+    debugger
+     this.image = new Image();
+     this.image.src = './images/background.gif';
+     ctx.drawImage(this.image, 0, 0);
+
+    this.image.onLoad = () => {
+      debugger
+      let x = 0;
+      let width = this.image.naturalWidth;
+      let min = 0 - width;
+      let count = 1;
+
+      const loop = () => {
+        ctx.drawImage(this.image, this.x, 0);
+        ctx.drawImage(this.image, this.x + width * 2 ,0);
+        ctx.drawImage(this.image, this.x + width * 3, 0);
+        x -= count;
+        if (x < min ) {
+          x = 0;
+        }
+      };
+
+
+      setInterval(loop,10);
+    };
+
+  }
+}
+
+
+module.exports = Background;
+
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=bundle.js.map
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+ entry: "./lib/legend_of_chibi.js",
+ output: {
+     filename: "./bundle.js"
+ },
+ devtool: 'source-map',
+};

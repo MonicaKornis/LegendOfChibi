fa8b76bb6c6775b66f06a1bf042011cc180f1755
monicakornis <2011mkornis@gmail.com>
2018-01-21 16:21:21 -0500

    adding additional token logic / death tokens do not work

--- bundle.js
+++ bundle.js
@@ -60,39 +60,23 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-const Game = __webpack_require__(1);
-
-document.addEventListener('DOMContentLoaded', () => {
-  let gameCanvas = document.getElementById('game-canvas');
-  let backgroundCanvas = document.getElementById('background-canvas');
-  let backgroundCtx = backgroundCanvas.getContext('2d');
-  let gameCtx = gameCanvas.getContext('2d');
-
-  const game = new Game(gameCtx, backgroundCtx, gameCanvas, backgroundCanvas);
-  // debugger
-  game.start();
-});
-
-
-/***/ }),
-/* 1 */
-/***/ (function(module, exports, __webpack_require__) {
-
-let Player = __webpack_require__(4);
+let Player = __webpack_require__(1);
 let Background = __webpack_require__(3);
-let Token = __webpack_require__(5);
-let Alien = __webpack_require__(7);
+let Token = __webpack_require__(4);
+let Alien = __webpack_require__(5);
 
 class Game {
   constructor(gameCtx,backgroundCtx,gameCanvas,backgroundCanvas) {
     this.player = new Player(12,12,2 * Math.PI,'red');
+    this.heart = new Image();
+    this.heart.src = './images/heart.png';
     this.alien = new Alien();
     this.tokens = [];
     this.gameCtx = gameCtx;
@@ -103,17 +87,19 @@ class Game {
     this.backgroundCanvas = backgroundCanvas;
     this.move = this.move.bind(this);
     this.draw = this.draw.bind(this);
+    this.printHeart = this.printHeart.bind(this);
     this.renderTokens = this.renderTokens.bind(this);
+    // this.createEvilTokens =  this.createEvilTokens.bind(this);
     this.createTokens = this.createTokens.bind(this);
     this.playerRecievePoints = this.playerRecievePoints.bind(this);
     this.createTokens(8);
     this.setEventListeners();
-    debugger
+    this.difficulty = 2;
     this.aliens = [];
     this.fps = 30;
     this.now;
     this.then = Date.now();
-    this.interval = 1500/this.fps;
+    this.interval = 3500/this.fps;
     this.delta;
 
   }
@@ -123,27 +109,49 @@ class Game {
   }
 
   createTokens(num) {
-    for (let i = 0; i < num; i++) {
-      this.tokens.push( new Token(this.gameCtx,i));
+    for (let points = 0; points < num; points++) {
+      if(points === 0) {
+
+      for(let i = 0; i < this.difficulty ; i++) {
+        debugger
+        this.tokens.push( new Token(this.gameCtx, -5, 'death-token'));
+        }
+
+      } else {
+        this.tokens.push( new Token(this.gameCtx, points, 'token'));
+      }
     }
   }
 
-  playerRecievePoints() {
+  // createEvilTokens() {
+  //   for (let i = 0; i <= this.difficulty; i++) {
+  //     // debugger
+  //     this.tokens.push( new Token(this.gameCtx, i ,'death token'));
+  //   }
+  // }
 
+  playerRecievePoints() {
+    let recieved = false;
     for (var i = 0; i < this.tokens.length; i++) {
-        if((this.tokens[i].xCoord >= this.player.centerX && this.tokens[i].xCoord  <= this.player.centerX + 40) &&
-           (this.tokens[i].yCoord >= this.player.centerY && this.tokens[i].yCoord  <= this.player.centerY + 40)) {
+        if((this.tokens[i].xCoord >= this.player.centerX && this.tokens[i].xCoord <= this.player.centerX + 40) &&
+           (this.tokens[i].yCoord >= this.player.centerY && this.tokens[i].yCoord <= this.player.centerY + 40)) {
           console.log('WEEE');
           this.player.points += this.tokens[i].points;
           this.tokens = this.tokens.filter((currentToken) => currentToken.yCoord != this.tokens[i].yCoord);
+          this.printHeart();
         }
     }
-
   }
+
+printHeart() {
+  this.gameCtx.drawImage(this.heart,0,0,
+  this.heart.naturalWidth, this.heart.naturalHeight, this.player.centerX + 20 , this.player.centerY -30,
+  this.heart.naturalWidth, this.heart.naturalHeight);
+}
 
   renderTokens(gameCtx) {
       this.tokens.forEach( function(token) {
-        token.xCoord -= 1;
+        token.xCoord -= 2;
         token.draw(gameCtx);
       });
   }
@@ -154,7 +162,9 @@ class Game {
     }
   }
 
-
+  filterTokens() {
+    this.tokens = this.tokens.filter((token) => token.xCoord > 4);
+  }
 
   move(e) {
     e.preventDefault();
@@ -180,13 +190,14 @@ class Game {
     requestAnimationFrame(this.start);
     let now = Date.now();
     this.delta = now - this.then;
-
+    // debugger
     if (this.delta > this.interval) {
-        this.gameCtx.clearRect(0,0,680,650);
+      // this.alien.xCoord -=1;
+      this.gameCtx.clearRect(0,0,680,650);
+      this.alien.draw(this.gameCtx);
         this.renderBackground(this.backgroundCtx);
         this.draw(this.gameCtx);
         this.renderTokens(this.gameCtx);
-        this.alien.draw(this.gameCtx);
         this.then = now - (this.delta % this.interval);
       }
     this.createMoreTokens();
@@ -194,8 +205,8 @@ class Game {
 
   draw() {
     // debugger
-    this.player.draw(this.gameCtx);
-    this.playerRecievePoints();
+    this.player.draw(this.gameCtx);this.playerRecievePoints();
+    this.filterTokens();
     this.gameCtx.font = `48px sans-serif`;
     let gradient = this.gameCtx.createLinearGradient(0,0,this.gameCanvas.width,0);
     gradient.addColorStop("0","magenta");
@@ -203,6 +214,7 @@ class Game {
     gradient.addColorStop("1.0","red");
     this.gameCtx.fillStyle = gradient;
     this.gameCtx.fillText(`Life points: ${this.player.points}`, 10, 50);
+
   }
 
 }
@@ -211,54 +223,10 @@ module.exports = Game;
 
 
 /***/ }),
-/* 2 */,
-/* 3 */
+/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-let Player = __webpack_require__(4);
-
-class Background {
-  constructor() {
-    this.x = 0;
-  }
-
-
-  draw(ctx) {
-    this.image = new Image();
-
-    this.image.onload = function() {
-      // let x = 0;
-      let width = this.image.naturalWidth;
-      let min = 0 - width;
-      let count = 3;
-
-      const loop = () => {
-        // debugger
-
-        ctx.drawImage(this.image, this.x, 0);
-        ctx.drawImage(this.image, this.x + width * 1.001 ,0);
-        ctx.drawImage(this.image, this.x + width * 1.002, 0);
-
-        this.x -= count;
-        if (this.x < min ) {
-          this.x = 0;
-        }
-      };
-
-      this.interval = this.interval || setInterval(loop.bind(this),17);
-    }.bind(this);
-
-    this.image.src = './images/background.gif';
-  }
-}
-
-
-module.exports = Background;
-
-
-/***/ }),
-/* 4 */
-/***/ (function(module, exports) {
+let Game = __webpack_require__(0);
 
 class Player {
   constructor(centerX, centerY, radius, color, ctx) {
@@ -295,7 +263,6 @@ class Player {
     }
   }
 
-
   draw(ctx) {
     ctx.drawImage(this.spriteSheet,0,0,
     this.frameWidth, this.frameHeight, this.centerX, this.centerY,
@@ -309,22 +276,87 @@ module.exports = Player;
 
 
 /***/ }),
-/* 5 */
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const Game = __webpack_require__(0);
+
+document.addEventListener('DOMContentLoaded', () => {
+  let gameCanvas = document.getElementById('game-canvas');
+  let backgroundCanvas = document.getElementById('background-canvas');
+  let backgroundCtx = backgroundCanvas.getContext('2d');
+  let gameCtx = gameCanvas.getContext('2d');
+
+  const game = new Game(gameCtx, backgroundCtx, gameCanvas, backgroundCanvas);
+  // debugger
+  game.start();
+});
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+let Player = __webpack_require__(1);
+
+class Background {
+  constructor() {
+    this.x = 0;
+  }
+
+
+  draw(ctx) {
+    this.image = new Image();
+
+    this.image.onload = function() {
+      // let x = 0;
+      let width = this.image.naturalWidth;
+      let min = 0 - width;
+      let count = 7;
+
+      const loop = () => {
+        // debugger
+
+        ctx.drawImage(this.image, this.x, 0);
+        ctx.drawImage(this.image, this.x + width * 1.001 ,0);
+        ctx.drawImage(this.image, this.x + width * 1.002, 0);
+
+        this.x -= count;
+        if (this.x < min ) {
+          this.x = 0;
+        }
+      };
+
+      this.interval = this.interval || setInterval(loop.bind(this),17);
+    }.bind(this);
+
+    this.image.src = './images/background.gif';
+  }
+}
+
+
+module.exports = Background;
+
+
+/***/ }),
+/* 4 */
 /***/ (function(module, exports) {
 
 class Token {
-  constructor(ctx,points){
+  constructor(ctx,points,type){
     this.points = points;
+    this.index = points === -5 ? 0 : points;
     this.ctx = ctx;
     this.tokenSheet = new Image();
     this.tokenSheet.onload = () => {
 
-      this.tokenSheet.src = './images/foods.png';
+      this.tokenSheet.src = './images/potions.png';
     };
 
-    this.tokenSheet.src = './images/foods.png';
-    this.width = 500/8;
-    this.height = 61;
+    this.type = type;
+    this.tokenSheet.src = './images/potions.png';
+    this.width = type == 'token' ? 255/5 : 51;
+    this.height = type == 'token' ? 63 : 51;
     this.startX = 1;
     this.xCoord = Math.floor(Math.random() * 620) + 100;
     this.yCoord = Math.floor(Math.random() * 620) + 100;
@@ -333,7 +365,9 @@ class Token {
 
   draw(ctx) {
     if(this.tokenSheet.src) {
-      ctx.drawImage(this.tokenSheet, this.points * this.width, 0,
+      // ctx.fillStyle = 'green';
+      // ctx.fillRect(this.xCoord, this.yCoord, this.width, this.height);
+      ctx.drawImage(this.tokenSheet, this.index * this.width, 0,
       this.width, this.height, this.xCoord, this.yCoord,
       this.width/1.5, this.height/1.5);
     }
@@ -345,8 +379,7 @@ module.exports = Token;
 
 
 /***/ }),
-/* 6 */,
-/* 7 */
+/* 5 */
 /***/ (function(module, exports) {
 
 class Alien {
@@ -357,10 +390,11 @@ class Alien {
     this.height = 64;
     this.frame = 0;
     this.draw = this.draw.bind(this);
+    this.xcoord = Math.floor(Math.random() * 620) + 100;
+    this.yCoord = Math.floor(Math.random() * 620) + 100;
   }
 
   draw(ctx) {
-    // debugger
       ctx.clearRect(400,500, this.frameWidth, this.frameHeight);
       ctx.drawImage(this.alienSheet,this.width*this.frame,0,
       this.width, this.height, 400,500,this.width, this.height);
